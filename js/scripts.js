f6015d694cd7c600571462d722d12aa0cf4b4f32
Ufuk Angay <u.angay@gmail.com>
2021-02-24 14:49:20 +0100

    Sending Push-Notifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
 //See if the browser supports Service Workers, if so try to register one
 if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("service-worker.js").then(function(registering){
-      // Registration was successful
-      console.log("Browser: Service Worker registration is successful with the scope",registering.scope);
-    }).catch(function(error){
-      //The registration of the service worker failed
-      console.log("Browser: Service Worker registration failed with the error",error);
-    });
-  }else { 
+  navigator.serviceWorker.register("service-worker.js").then(function(registering){
+    // Registration was successful
+    console.log("Browser: Service Worker registration is successful with the scope",registering.scope);
+  }).catch(function(error){
     //The registration of the service worker failed
-    console.log("Browser: I don't support Service Workers :(");
+    console.log("Browser: Service Worker registration failed with the error",error);
+  });
+
+  //Asking for permission with the Notification API
+if(typeof Notification!==typeof undefined){ //First check if the API is available in the browser
+Notification.requestPermission().then(function(result){ 
+  //If accepted, then save subscriberinfo in database
+  if(result==="granted"){
+    console.log("Browser: User accepted receiving notifications, save as subscriber data!");
+    navigator.serviceWorker.ready.then(function(serviceworker){ //When the Service Worker is ready, generate the subscription with our Serice Worker's pushManager and save it to our list
+      const VAPIDPublicKey="BPXdgtvn66KmDElqRrUJHALfLrjfMWszNmxPsSzPS7gIMcuymZgxcCID2yg0D6Dn8qivr9z0VCKuC9y80ddSCV4"; // Fill in your VAPID publicKey here
+      const options={applicationServerKey:VAPIDPublicKey,userVisibleOnly:true} //Option userVisibleOnly is neccesary for Chrome
+      serviceworker.pushManager.subscribe(options).then((subscription)=>{
+        //POST the generated subscription to our saving script (this needs to happen server-side, (client-side) JavaScript can't write files or databases)
+        let subscriberFormData=new FormData();
+        subscriberFormData.append("json",JSON.stringify(subscription));
+        fetch("data/saveSubscription.php",{method:"POST",body:subscriberFormData});
+      }).catch((error)=>{
+        console.log(error);
+      });
+    });
   }
+}).catch((error)=>{
+  console.log(error);
+});
+}
+
+
+}else { 
+  //The registration of the service worker failed
+  console.log("Browser: I don't support Service Workers :(");
+}
--- service-worker.js
+++ service-worker.js
@@ -61,4 +61,21 @@ self.addEventListener("fetch",(fetching)=>{
 
 self.addEventListener("push",(pushing)=>{
 	console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
+  if(pushing.data){
+    pushdata=JSON.parse(pushing.data.text());		
+    console.log("Service Worker: I received this:",pushdata);
+    if((pushdata["title"]!="")&&(pushdata["message"]!="")){			
+      const options={ body:pushdata["message"] }
+      self.registration.showNotification(pushdata["title"],options);
+      console.log("Service Worker: I made a notification for the user");
+    } else {
+      console.log("Service Worker: I didn't make a notification for the user, not all the info was there :(");			
+    }
+  }
 })
+
+self.addEventListener("notificationclick",function(clicking){
+  const pageToOpen="/";
+	const promiseChain=clients.openWindow(pageToOpen);
+	event.waitUntil(promiseChain);
+});
